b38f605edc6c2434c2b44270fa5d3a109600bc82
Nazir Knuckles <nazirknuckles@gmail.com>
2024-04-18 02:48:17 -0400

    Organized the file structure a little more

--- server/server.js
+++ server/server.js
@@ -1,34 +1,25 @@
-const express = require('express');
-const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
-const path = require("path");
-const { authMiddleware } = require("./utils/auth");
-
 const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require("@apollo/server");
+const { authMiddleware } = require("./utils/auth");
 const db = require('./config/connection');
-
+const express = require('express');
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
 
 // Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({ typeDefs, resolvers });
+
 const startApolloServer = async () => {
   try {
     await server.start();
   
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
-  
-    // Serve up static assets
-    app.use('/images', express.static(path.join(__dirname, '../client/images')));
-  
-    app.use('/graphql', expressMiddleware(server, {
-      context: authMiddleware
-    }));
+
+    app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
   
     if (process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -40,7 +31,6 @@ const startApolloServer = async () => {
   
     db.once('open', () => {
       app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
       });
     });
@@ -51,4 +41,3 @@ const startApolloServer = async () => {
 };
 
 startApolloServer();
-
