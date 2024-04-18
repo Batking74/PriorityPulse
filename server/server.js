affe5815c754d920ddfd64d82782f2ca04ab25f5
Nazir Knuckles <nazirknuckles@gmail.com>
2024-04-18 03:09:16 -0400

    Commented out database

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const { expressMiddleware } = require("@apollo/server/express4");
 const { typeDefs, resolvers } = require('./schemas');
 const { ApolloServer } = require("@apollo/server");
 const { authMiddleware } = require("./utils/auth");
-const db = require('./config/connection');
+// const db = require('./config/connection');
 const express = require('express');
 const path = require("path");
 const app = express();
@@ -15,25 +15,25 @@ const server = new ApolloServer({ typeDefs, resolvers });
 const startApolloServer = async () => {
   try {
     await server.start();
-  
+
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
     app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
-  
+
     if (process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, '../client/dist')));
-  
+
       app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
       });
     }
-  
-    db.once('open', () => {
-      app.listen(PORT, () => {
-        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-      });
+    
+    app.listen(PORT, () => {
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
+
+    // db.once('open', () => {});
   }
   catch (error) {
     console.error('Error starting Apollo Server:', error);
